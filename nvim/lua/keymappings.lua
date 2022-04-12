51d8ef397b5060daa70ad510c97a19c71b587d4d
Joakim Paulsson <joakim.jp.paulsson@gmail.com>
2022-04-12 11:32:23 +0200

    changed telescope mappings to be in keymappings.lua instead

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,3 +1,8 @@
 local utils = require('utils')
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jk', '<Esc>')           -- jk to escape
+
+utils.map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+utils.map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+utils.map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+utils.map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
